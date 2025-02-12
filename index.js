3886d2172143af2f6093796aecbe506322fdfe98
Emilia-Dumitru <dumitruemilia41@gmail.com>
2025-02-12 17:31:21 +0200

    finished project

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-const apiKey = "xnDtAGLr3oJbQN6/fwj0mA==huORsh7XqS7Iq1Md";
-const url = "https://www.api-ninjas.com/api/quotes";
-
 async function getRandomQuote() {
     try {
-        const response = await fetch(url, {
-            method: "GET",
-            headers: { "X-Api-Key": apiKey }
-        });
+        const response = await fetch("http://api.quotable.io/random"); 
+
         if (!response.ok) {
             throw new Error("Failed to fetch quote");
         }
-        const data = await response.json();
-        const quoteText = data[0].quote;
 
-        document.querySelector(".initial-p").innerHTML = `<span>"</span>${quoteText}<span>"</span>`;
+        const data = await response.json();
+        document.querySelector(".initial-p").innerHTML = `<span>"</span>${data.content}<span>"</span>`;
     } catch (error) {
         console.error("Error fetching quote:", error);
         document.querySelector(".initial-p").textContent = "Failed to load quote. Try again!";
